65e55a14f56a86b43bd5d5d25feec49f01eff30d
Vladyslav <vladyslav.lutsenko@nure.ua>
2021-12-01 10:48:57 +0200

    init at index.js: db connection, bodyParser, middlewares

--- index.js
+++ index.js
@@ -1,23 +1,26 @@
 const express = require('express')
 const cors = require('cors')
+const bodyParser = require("body-parser")
+const middlewares = require('middlewares')
 
 const app = express()
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(cors())
 
-const catalog = require('./app/routes/catalog')
+require('src/database/connection')
+
+const catalog = require('./src/routes/catalog')
 app.use('/catalog', catalog)
 
 const basket = require('./src/routes/basket')
 app.use('/basket', basket)
 
-// const basket = require('./app/routes/basket')
-// app.use('/basket', basket)
-
-// const order = require('./app/routes/order')
-// app.use('/order', order)
-
+app.use(middlewares.notFound);
+app.use(middlewares.errorHandler);
 
 module.exports = app
