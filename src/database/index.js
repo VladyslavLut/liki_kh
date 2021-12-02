f632445c02caa2bf5cd47db32076402eb32a8e19
Vladyslav <vladyslav.lutsenko@nure.ua>
2021-12-02 02:32:29 +0200

    simplify entities relations declaration

--- src/database/index.js
+++ src/database/index.js
@@ -1,4 +1,4 @@
-const db = require('./connection')
+require('./connection')
 const Category = require('../models/category.model')
 const Ingredient = require('../models/ingredient.model')
 const Pharmacy = require('../models/pharmacy.model')
@@ -10,39 +10,23 @@ const MedicineIngredient = require('../models/medicine.ingredient.model')
 const MedicinePharmacy = require('../models/medicine.pharmacy.model')
 const MedicineOrder = require('../models/medicine.order.model')
 
-//Many medicines of one category
 Category.hasMany(Medicine, {foreignKey: 'category_id'})
 Medicine.belongsTo(Category, {foreignKey: 'category_id'})
 
-//Many medicines have many ingredients
-Medicine.hasMany(MedicineIngredient, {foreignKey: 'medicine_id'})
-MedicineIngredient.belongsTo(Medicine, {foreignKey: 'medicine_id'})
+Medicine.belongsToMany(Ingredient, {through: MedicineIngredient})
+Ingredient.belongsToMany(Medicine, {through: MedicineIngredient})
 
-Ingredient.hasMany(MedicineIngredient, {foreignKey: 'ingredient_id'})
-MedicineIngredient.belongsTo(Ingredient, {foreignKey: 'ingredient_id'})
+Medicine.belongsToMany(Pharmacy, {through: MedicinePharmacy})
+Pharmacy.belongsToMany(Medicine, {through: MedicinePharmacy})
 
-//Many pharmacies have many medicines
-Medicine.hasMany(MedicinePharmacy, {foreignKey: 'medicine_id'})
-MedicinePharmacy.belongsTo(Medicine, {foreignKey: 'medicine_id'})
+MedicinePharmacy.hasMany(Basket)
+Basket.belongsTo(MedicinePharmacy)
 
-Pharmacy.hasMany(MedicinePharmacy, {foreignKey: 'pharmacy_id'})
-MedicinePharmacy.belongsTo(Pharmacy, {foreignKey: 'pharmacy_id'})
+Pharmacy.hasMany(Order)
+Order.belongsTo(Pharmacy)
 
-// Many baskets have a medicine from exact pharmacy
-MedicinePharmacy.hasMany(Basket, {foreignKey: 'medicine_pharmacy_id'})
-Basket.belongsTo(MedicinePharmacy, {foreignKey: 'medicine_pharmacy_id'})
+Client.hasMany(Order)
+Order.belongsTo(Client)
 
-//Many orders belong to one pharmacy
-Pharmacy.hasMany(Order, {foreignKey: 'pharmacy_id'})
-Order.belongsTo(Pharmacy, {foreignKey: 'pharmacy_id'})
-
-//Many orders belong to one client
-Client.hasMany(Order, {foreignKey: 'client_id'})
-Order.belongsTo(Client, {foreignKey: 'client_id'})
-
-//Many orders contain many medicines
-Order.hasMany(MedicineOrder, {foreignKey: 'order_id'})
-MedicineOrder.belongsTo(Order, {foreignKey: 'order_id'})
-
-Medicine.hasMany(MedicineOrder, {foreignKey: 'medicine_id'})
-MedicineOrder.belongsTo(Medicine, {foreignKey: 'medicine_id'})
+Order.belongsToMany(Medicine, {through: MedicineOrder})
+Medicine.belongsToMany(Order, {through: MedicineOrder})
