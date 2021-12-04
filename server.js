1185d57842d48660061bc07200f9939e8d0e7892
Vladyslav <vladyslav.lutsenko@nure.ua>
2021-12-04 23:30:57 +0200

    run server on env port

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const app = require('./index')
+const port = process.env.PORT || 3000
 
-app.listen(process.env.SERVER_PORT || 3000, (err) => {
+app.listen(port, (err) => {
     if (err) throw err
-    console.log(`Server is running on http://127.0.0.1:3000`)
+    console.log(`Server is running on port:${port}`)
 })
